24f97e23ecf470cefaa191b3feb93b1f09ddc1a9
zamani <a.zamani697@gmail.com>
2019-05-19 09:58:08 +0430

    update

--- src/app/invited-firend/invited-firend.component.ts
+++ src/app/invited-firend/invited-firend.component.ts
@@ -7,15 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitedFirendComponent implements OnInit {
   txt:string;
+  regcode:string;
   constructor() { }
 
   ngOnInit() {
-    let regcode= JSON.parse(localStorage.getItem("Profile")).reagentCode;
+    this.regcode= JSON.parse(localStorage.getItem("Profile")).reagentCode;
     this.txt="سلام یارباکس اپلیکیشن سریع، ساده و کم هزینه ایست که"+
    "من برای ارسال خرده بار هام به شهرهای مختلف ازش استفاده "+
    "می کنم. به شما هم پیشنهاد می کنم از طریق کا فه بازار  یارباکس رو دانلود کنی "+
-   "کد معرف :   "+ regcode + 
-   "https://yarbox.co/account/sign-up?ReagentCode="+regcode
+   "کد معرف :   "+ this.regcode + 
+   "https://yarbox.co/account/sign-up?ReagentCode="+this.regcode
    
   }
 
