8d9072ca3d599ac37aaa55e17bfda274dc580149
zamani <a.zamani697@gmail.com>
2020-02-06 10:23:01 +0330

    css

--- src/app/Services/webapp-service.ts
+++ src/app/Services/webapp-service.ts
@@ -15,8 +15,8 @@ private packsRuning:any;
 constructor(
   private _http:HttpClient,
   private _packService:PackService){
- this.api="http://localhost:11926/api/vv2/";
- // this.api="https://api.yarbox.co/api/vv2/";
+  this.api="http://localhost:11926/api/vv2/";
+  //this.api="https://api.yarbox.co/api/vv2/";
 }
  
 
